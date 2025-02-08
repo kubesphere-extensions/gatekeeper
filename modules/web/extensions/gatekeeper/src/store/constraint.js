e3ef09df398ba2584bb09b50288ec222a5589c57
lannyfu <lannyfu@kubesphere.io>
2025-02-08 15:53:23 +0800

    feat: Support set parameters default value
    Signed-off-by: lannyfu <lannyfu@kubesphere.io>
    (cherry picked from commit 57140701c4569e3fc65112fe35a2866c1ec25912)

--- modules/web/extensions/gatekeeper/src/store/constraint.js
+++ modules/web/extensions/gatekeeper/src/store/constraint.js
@@ -1,16 +1,16 @@
-import { BaseStore, getPath, request } from '@ks-console/shared'
-import { find, get, isEmpty } from 'lodash'
-import { API_VERSIONS } from '../utils/constants'
+import { BaseStore, getPath, request } from '@ks-console/shared';
+import { find, get, isEmpty } from 'lodash';
+import { API_VERSIONS } from '../utils/constants';
 
-const module = 'constraints'
+const module = 'constraints';
 
 const mapper = item => {
   return {
     name: item.metadata.name || item.items[0].metadata.name,
     uid: item.metadata.uid,
     ...item,
-  } 
-}
+  };
+};
 
 const yamlRawData = `apiVersion: constraints.gatekeeper.sh/v1beta1
 kind: K8sPSPHostFilesystem
@@ -30,7 +30,7 @@ spec:
 #      matchLabels:
 #        kubesphere.io/workspace: system-workspace
   parameters: {}
-`
+`;
 
 const getResourceUrl = (params, ksVersion) => {
   if (ksVersion) {
@@ -41,35 +41,73 @@ const getResourceUrl = (params, ksVersion) => {
     }
     return `${API_VERSIONS[module]}${getPath(params)}`;
   }
-}
+};
 
-const fetchConstraintKind = async (params) => {
-  const url = `${API_VERSIONS['constrainttemplates']}${getPath(params)}/constrainttemplates`
-  const result = await request.get(url)
-  const items = get(result, 'items', [])
+const fetchConstraintKind = async params => {
+  const url = `${API_VERSIONS['constrainttemplates']}${getPath(params)}/constrainttemplates`;
+  const result = await request.get(url);
+  const items = get(result, 'items', []);
   return {
     data: items,
     total: items.length,
-  }
-}
+  };
+};
 
-const checkNameFn = async ({ name, kind,cluster }) => {
+const checkNameFn = async ({ name, kind, cluster }) => {
   if (kind) {
-    const url = `kapis/constraints.gatekeeper.sh/v1beta1${getPath({cluster})}/${module}`
-    const result = await request.get(url)
-    const items = get(result, 'items', [])
-    return {exist:!isEmpty(find(items, (item)=>item.metadata.name===name&&item.kind===kind))}
+    const url = `kapis/constraints.gatekeeper.sh/v1beta1${getPath({ cluster })}/${module}`;
+    const result = await request.get(url);
+    const items = get(result, 'items', []);
+    return {
+      exist: !isEmpty(find(items, item => item.metadata.name === name && item.kind === kind)),
+    };
   }
-  return {exist:false}
-}
+  return { exist: false };
+};
 
+const generateExampleWithComments = (schema, indent = 0) => {
+  let yamlString = '';
+
+  if (schema.type === 'object') {
+    for (const key in schema.properties) {
+      const prop = schema.properties[key];
+
+      if (prop.description) {
+        const lines = prop.description.split('\n');
+        lines.forEach(line => {
+          yamlString += `${' '.repeat(indent)}# ${line}\n`;
+        });
+      }
+      yamlString += `${' '.repeat(indent)}${key}: ${generateExampleWithComments(prop, indent + 2)}`;
+    }
+  } else if (schema.type === 'array') {
+    if (schema.items.description) {
+      const lines = schema.items.description.split('\n');
+      lines.forEach(line => {
+        yamlString += `${' '.repeat(indent)}# ${line}\n`;
+      });
+    }
+    const exampleItem = generateExampleWithComments(schema.items, indent + 2);
+    yamlString += `\n${' '.repeat(indent)}- ${exampleItem.trim()}\n`;
+  } else if (schema.type === 'string') {
+    yamlString += `"example-string"\n`;
+  } else if (schema.type === 'integer') {
+    yamlString += `123\n`;
+  } else if (schema.type === 'boolean') {
+    yamlString += `true\n`;
+  } else {
+    yamlString += `null\n`;
+  }
+
+  return yamlString;
+};
 
 const { ...baseStore } = BaseStore({
   module,
   mapper,
   getListUrlFn: getResourceUrl,
   getResourceUrlFn: getResourceUrl,
-})
+});
 
 const constraintStore = {
   ...baseStore,
@@ -78,7 +116,7 @@ const constraintStore = {
   fetchConstraintKind,
   yamlRawData,
   checkNameFn,
-}
+  generateExampleWithComments,
+};
 
-export default constraintStore
-
+export default constraintStore;
